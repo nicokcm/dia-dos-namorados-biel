2b78be26b7f4942bdd4e4b0011ab7b06ca68f82f
Nicolas Kucham <90403724+nicokcm@users.noreply.github.com>
2026-06-02 20:18:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ contador.innerHTML = dias + " dias ❤️";
 setInterval(atualizarContador,1000);
 
 atualizarContador();
+
+function escolherVoce(){
+
+document.getElementById("mensagemFinal").style.display = "block";
+
+for(let i=0;i<40;i++){
+
+const coracao = document.createElement("div");
+
+coracao.innerHTML = "❤️";
+
+coracao.classList.add("coracao");
+
+coracao.style.left = Math.random() * 100 + "vw";
+
+coracao.style.bottom = "-20px";
+
+document.body.appendChild(coracao);
+
+setTimeout(()=>{
+coracao.remove();
+},4000);
+
+}
+
+}
